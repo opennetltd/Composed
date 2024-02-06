325693f080f3dde62ff2634ca19d8c3a55f03f4f
Joseph Duffy <commits@josephduffy.co.uk>
2024-02-06 14:12:45 -0800

    Improve debug description of `Changeset`

--- Sources/ComposedUI/Common/Changeset.swift
+++ Sources/ComposedUI/Common/Changeset.swift
@@ -3,7 +3,7 @@ import Foundation
 /**
  A collection of changes to be applied in batch.
  */
-public struct Changeset {
+public struct Changeset: CustomDebugStringConvertible {
     public struct Move: Hashable {
         public var from: IndexPath
         public var to: IndexPath
@@ -21,4 +21,22 @@ public struct Changeset {
     public var elementsInserted: Set<IndexPath> = []
     public var elementsMoved: Set<Move> = []
     public var elementsUpdated: Set<IndexPath> = []
+
+    public var debugDescription: String {
+        func compareMoves(lhs: Move, rhs: Move) -> Bool {
+            lhs.from < rhs.from
+        }
+
+        return """
+        Changeset(
+            groupsInserted: \(groupsInserted.sorted(by: <)),
+            groupsRemoved: \(groupsRemoved.sorted(by: <)),
+            groupsUpdated: \(groupsUpdated.sorted(by: <)),
+            elementsRemoved: \(elementsRemoved.sorted(by: <)),
+            elementsInserted: \(elementsInserted.sorted(by: <)),
+            elementsMoved: \(elementsMoved.sorted(by: compareMoves(lhs:rhs:))),
+            elementsUpdated: \(elementsUpdated.sorted(by: <)),
+        )
+        """
+    }
 }
